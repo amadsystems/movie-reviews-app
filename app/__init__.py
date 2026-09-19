ddbf2f4042a3ccd20d2ab7aae49072bf13cb5ede
amadsystems <armanmadatyancs@gmail.com>
2026-09-19 11:17:04 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,25 @@
+import os
 
+from dotenv import load_dotenv
+from flask import Flask, request
+
+from config import DevConfig
+from app.movies.routes import movies_bp
+from app.reviews.routes import reviews_bp
+
+
+def create_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+    app.config.from_object(DevConfig)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+    app.register_blueprint(movies_bp, url_prefix="/movies")
+    app.register_blueprint(reviews_bp, url_prefix="/reviews")
+
+    @app.before_request
+    def log_request():
+        print(f"{request.method} {request.path}")
+
+    return app
