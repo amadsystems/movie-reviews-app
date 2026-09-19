83eb811f61387d2a3e21acb4d3235bf65d3e12ac
amadsystems <armanmadatyancs@gmail.com>
2026-09-19 11:23:19 -0700

    Update routes.py

--- app/reviews/routes.py
+++ app/reviews/routes.py
@@ -1,22 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 reviews_bp = Blueprint("reviews", __name__)
 
-REVIEWS = [
-    {"id": 1, "movie_id": 1, "rating": 5},
-    {"id": 2, "movie_id": 2, "rating": 4},
+reviews = [
+    {"id": 1, "movie": "Spider-Man", "rating": 5},
+    {"id": 2, "movie": "Shrek", "rating": 4},
+    {"id": 3, "movie": "Cars", "rating": 5}
 ]
 
 
 @reviews_bp.route("/")
 def list_reviews():
-    return jsonify({"data": REVIEWS})
+    return {"data": reviews}
 
 
 @reviews_bp.route("/<int:review_id>")
 def get_review(review_id):
-    for review in REVIEWS:
+    for review in reviews:
         if review["id"] == review_id:
-            return jsonify(review)
+            return review
 
-    return jsonify({"error": "review not found"}), 404
+    return {"error": "Review not found"}, 404
